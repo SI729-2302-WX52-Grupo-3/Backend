2533125bce83b2c32838f8b084922e3cec0646de
AngieV <89101151+AngieValverdeSalazar@users.noreply.github.com>
2023-11-04 03:25:41 -0500

    Create PaymentDetail.java

---
+++ src/main/java/com/example/medicare/api/appointment/domain/model/entities/PaymentDetail.java
@@ -0,0 +1,39 @@
+package com.example.repair.repair.appointment.domain.model.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "payment_details")
+public class PaymentDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Min(value = 0)
+    @Column(name = "subtotal", columnDefinition = "decimal(10,2)")
+    private double subtotal;
+
+    @NotNull
+    @Min(value = 0)
+    @Column(name = "discount", columnDefinition = "decimal(10,2)")
+    private double discount;
+
+    @NotNull
+    @Min(value = 0)
+    @Column(name = "retention_percentage", columnDefinition = "decimal(10,2)")
+    private double retention_percentage;
+
+    @NotNull
+    @Min(value = 0)
+    @Column(name = "retention_amount", columnDefinition = "decimal(10,2)")
+    private double retention_amount;
+
+}
