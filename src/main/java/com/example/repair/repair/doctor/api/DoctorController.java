df8d5b4a84fa6174e6c3bf7261e8248b9ca40c5f
w1llyVR <wvaleric@gmail.com>
2023-11-03 23:07:29 -0500

    Bounded Context Doctor

---
+++ src/main/java/com/example/repair/repair/doctor/api/DoctorController.java
@@ -0,0 +1,41 @@
+package com.example.repair.repair.doctor.api;
+
+import com.example.repair.repair.doctor.domain.model.entities.Doctor;
+import com.example.repair.repair.doctor.domain.service.DoctorService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("doctors")
+public class DoctorController {
+
+    private final DoctorService doctorService;
+
+    @GetMapping
+    public List<Doctor> fetchAll() {
+        return doctorService.fetchAll();
+    }
+
+    @PostMapping
+    public Doctor save(@RequestBody Doctor doctor) {
+        return doctorService.save(doctor);
+    }
+
+    @PutMapping("/{id}")
+    public Doctor update(@RequestBody Doctor doctor, @PathVariable("id") Integer id) {
+        return doctorService.update(doctor, id);
+    }
+
+    @GetMapping("/{id}")
+    public Doctor fetchById(@PathVariable("id") Integer id) {
+        return doctorService.fetchById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteById(@PathVariable("id") Integer id) {
+        return doctorService.deleteById(id);
+    }
+}
